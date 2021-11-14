6be68e6b3c4f3ec99e7125961a5ac393c18cb0af
zoe <53597160+zoeniu@users.noreply.github.com>
2021-11-14 16:33:14 +1300

    Convert hex to rgb

--- index.js
+++ index.js
@@ -16,4 +16,25 @@ const isValidHex = (hex) => {
     return strippedHex.length === 3 || strippedHex.length === 6;
 }
 
-console.log(isValidHex(hexInput.value));
+//Create a function to convert Hex to RGB
+//this should work with 3 or 6 character hex values
+//Hint - useParseInt(16) to convert a hex value to a decimal value
+//should return an object with 3 properties - r,g, and b
+//Test your function with a few different use cases
+
+const hexToRGB = (hex) => {
+    if(!isValidHex(hex)) return null;
+
+    let strippedHex = hex.replace("#", "");
+    if (strippedHex.length === 3) {
+        strippedHex = strippedHex[0] + strippedHex[0]
+        + strippedHex[1] + strippedHex[1]
+        + strippedHex[2] + strippedHex[2];
+    }
+    const r = parseInt(strippedHex.substring(0, 2), 16);
+    const g = parseInt(strippedHex.substring(2, 4), 16);
+    const b = parseInt(strippedHex.substring(4, 6), 16);
+    return {r, g, b}
+}
+
+console.log(hexToRGB("123"));
